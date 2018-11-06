a4a8e6190376b16712ad6df0050a8a5e7dc9a50d
gott51010 <43871196+gott51010@users.noreply.github.com>
2018-11-06 15:36:00 +0900

    没卵用系列-算汇率升级版
    没卵用系列-算汇率升级版，现已支持5国币种相互换算

--- uselessSeries/meiluanyongmoney_gototo.php
+++ uselessSeries/meiluanyongmoney_gototo.php
@@ -10,16 +10,18 @@
 </head>
 <body>
 <?php session_start(); ?>
-<h1>低调奢华有内涵的汇率计算器~日元兑软妹币版</h1>
+<h1>低调奢华有内涵的汇率计算器~支持多国币种版</h1>
 
 
 
 <?php   
 $Amount = $_GET['Amount']; 
-// echo  $Amount;
+$from = $_GET['from'];
+$to = $_GET['to'];
 
+// print_r($_GET);
 
-$url ="http://qq.ip138.com/hl.asp?from=JPY&to=CNY&q=$Amount";   
+$url ="http://qq.ip138.com/hl.asp?from=$from&to=$to&q=$Amount";   
 $getcontent = file_get_contents($url); 
 $html = iconv("gb2312", "utf-8",$getcontent);
 
@@ -44,18 +46,6 @@ echo "换算后为:".$Amount*$rmb;
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
 // $regex = "/class=/"rate/" >(.+?) "."/i"; //正则表达式.   
 // if(preg_match_all($regex, $content, $matches)) {   
 // echo $Amount.' JPY = '.$matches[1][0].'CNY';   
@@ -63,10 +53,27 @@ echo "换算后为:".$Amount*$rmb;
 ?> 
 
 
-
+<p>将</p>
 <form>
+<select name="from">
+<option value="CNY">软妹币</option>
+<option value="JPY">日元</option>
+<option value="USD">美元</option>
+<option value="AUD">澳元</option>
+<option value="GBP">英镑</option>
+</select>
 
-请输入数字<input type='number' name="Amount">
+<p>兑换为</p>
+<select name="to">
+<option value="JPY">日元</option>
+<option value="CNY">软妹币</option>
+<option value="USD">美元</option>
+<option value="GBP">英镑</option>
+<option value="AUD">澳元</option>
+</select>
+
+<hr>
+兑换金额<input type='number' name="Amount">
 
 <input type="submit" value="开始计算">
 
